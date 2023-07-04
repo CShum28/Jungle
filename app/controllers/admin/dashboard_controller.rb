a7aee4e449275fd101ccdabf5ff3fdd6533946bc
Clement Shum <clement.shum128@gmail.com>
2023-07-04 14:06:39 -0400

    updated env to contain admin creds

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,3 +1,13 @@
+before_action :authenticate
+
+def authenticate
+  authenticate_or_request_with_http_basic do |username, password|
+    # Check the provided username and password for validity
+    # You can use any authentication mechanism here (e.g., comparing against stored credentials)
+    username == 'admin' && password == 'password'
+  end
+end
+
 class Admin::DashboardController < ApplicationController
   def show
   end
